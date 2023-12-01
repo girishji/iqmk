44b7bc83461ea4144dbe49ce22e229fe058b90f8
Girish Palya <girishji@gmail.com>
2023-12-01 20:55:35 +0000

    M  CMakeLists.txt A  cmake/qmk_core.cmake M  cmake/qmk_tests.cmake M  run.sh M  run_tests.sh D  tests/CMakeLists.txt D  tests/hidkb_test.cc

--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+rm -rf build
 mkdir -p build \
     && cd build \
-    && cmake .. \
-    && cmake --build . -j
+    && cmake ..
+
+if [[ $? -eq 0 ]]; then
+    if [[ $(uname -s) == "Darwin" ]]; then
+        cmake --build . -j 4
+    else
+        cmake --build .
+    fi
+fi
--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+rm -rf build
 mkdir -p build \
     && cd build \
-    && cmake -DQMK_BUILD_TYPE:STRING=Test .. \
-    && cmake --build . \
-    && ctest
+    && cmake -DQMK_BUILD_TYPE:STRING=Test ..
+
+if [[ $? -eq 0 ]]; then
+    if [[ $(uname -s) == "Darwin" ]]; then
+        cmake --build . -j 4
+    else
+        cmake --build .
+    fi
+fi
+
+[[ $? -eq 0 ]] && ctest
